dd470cb3685b7847cef984e9cb8fb54d729e4e33
aliefegulertekin <gulertekinaliefe@gmail.com>
2021-06-14 18:09:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,45 @@
-const currencySelection = document.getElementById('currency-selection');
-const exchangeSelection = document.getElementById('exchange-selection');
+const initialSelection = document.getElementById('currency-selection');
+const finalSelection = document.getElementById('exchange-selection');
 
 const calculateBtn = document.getElementById('calculate-button');
 const api_key = '526369d65eb4d58e7b114e5366677f69';
-let objects = {}
+let responseObjects = {};
 
-// fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=${api_key}`)
-// .then(response => {
-// response.json().then(data => {
-//   const {rates} = data; 
-//   for(const a in rates) {
-//     const b = document.createElement('option');
-//     const c = document.createElement('option');
-//     b.textContent = a;
-//     c.textContent = a;
-//     currencySelection.append(b);
-//     exchangeSelection.append(c);
-//   }
-//   objects = rates;
-// })  
-// })
-// currencySelection.addEventListener('change', () => {
-
-// })
 
 async function getData() {
 
 const response = await fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=${api_key}`);
 const data = await response.json();
 const {rates} = data
-rates.forEach(d => console.log(d));
-}
+for(const rate in rates) {
+      const initialCurrency = document.createElement('option');
+      const finalCurrency = document.createElement('option');
+      
+      initialCurrency.textContent = rate;
+      finalCurrency.textContent = rate;
+
+      initialSelection.append(initialCurrency);
+      finalSelection.append(finalCurrency);
+    }
+    responseObjects = rates;
+};
 
 
+const calculateCurrency = (initialInput,exchangeInput) => {
 
-// const calculateCurrency = (num) => {
-//   (inputValue * inputValue) / num;
-// }
+  
+  const x = responseObjects[initialSelection.value];
+  const y = responseObjects[finalSelection.value];
+  
+  exchangeInput.value= ((initialInput * y)/x).toFixed(2);
+};
 
-calculateBtn.addEventListener('click', () => {
-  const currencyInput = document.getElementById('currency-input').value;
+window.addEventListener('load', getData);
+calculateBtn.addEventListener('click',() => {
+  const initialInput = document.getElementById('currency-input').value;
   const exchangeInput = document.getElementById('exchange-input');
-
-  const x = objects[currencySelection.value];
-  const y = objects[exchangeSelection.value];
-  exchangeInput.value= ((currencyInput * y)/x).toFixed(2);
-
-})
-
-
-getData();
+  if(initialInput === "" || exchangeInput === "") {
+    return;
+  }
+  calculateCurrency(initialInput,exchangeInput);
+});  
